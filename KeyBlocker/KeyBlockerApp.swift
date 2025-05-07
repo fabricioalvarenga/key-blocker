95b59f5e6dc1b833aa3d9bcd88bd5cf9ccf41ec9
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-05-07 08:41:40 -0300

    Inital commit

---
+++ KeyBlocker/KeyBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyBlockerApp.swift
+//  KeyBlocker
+//
+//  Created by FABRICIO ALVARENGA on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
